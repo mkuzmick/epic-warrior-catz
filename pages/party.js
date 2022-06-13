0445265420e0e387eae149de094ddf5609efdf7e
mk <mk@mks-macbook-air.lan>
2022-06-13 17:39:08 -0400

    Updates: images added

---
+++ pages/party.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        uhhhhh
+      </h1>
+      <p>e e e  e e e e e e e e never gonna give you up</p>
+      <iframe width="800" height="500" src="https://www.youtube.com/embed/6Zbi0XmGtMw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  )  
+}
